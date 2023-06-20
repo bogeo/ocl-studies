819d4d1128dbbe47f5f984eec983bdcc889f6cad
Judith Kirstein <judith-kirstein@web.de>
2023-06-20 10:38:28 +0200

    Test aktualisiert

--- test.cmd
+++ test.cmd
@@ -1,25 +1,35 @@
 !create lp1 : LandParcel
+!lp1.id := '1'
+
 !create lp2 : LandParcel
+!lp2.id := '2'
+
 !create vp1 : ValidityPeriod
 !create vp2 : ValidityPeriod
+
 !create bl1 : BoundaryLine
 !create bl2 : BoundaryLine
-!create d1 : Date
-!create d2 : Date
-!create d2 : Date
+!create bl3 : BoundaryLine
 
-!lp1.id := '1'
-!lp2.id := '2'
+!create d1 : Date
 !d1.day := 1
 !d1.month := 1
 !d1.year := 2001
+
+!create d2 : Date
 !d2.day := 1
 !d2.month := 1
 !d2.year := 2023
 
+!create d3 : Date
+
+
+
 -- hier wird 'validTimePeriod' true:
 !vp1.startDate := d1
 !vp1.endDate := d2
+!vp2.startDate := d1
+!vp2.endDate := d2
 
 -- hier wird 'validTimePeriod' false:
 !vp2.startDate := d2
@@ -31,3 +41,9 @@
 !insert (lp2, bl1) into LandParcel_BoundaryLine
 !insert (bl1, lp1) into BoundaryLine_LandParcel
 !insert (bl1, lp1) into BoundaryLine_LandParcel
+
+-- pre oldParcelExisting ist true:
+!openter lp1 split(bl3)
+!opexit lp2
+-- die meisten Nachbedingungen sind false
+
